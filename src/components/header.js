db9f1dc92f0a48613340e883cc1a56daed23c4cd
S David Kong <SDAVIDKONG@gmail.com>
2022-05-30 16:40:19 +0200

    header

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Header() {
+    return (
+        <header>
+            <img src=""></img>
+            <h2>Header Component</h2>
+            <h4>React Course project 3</h4>
+            </header>
+    )
+}
