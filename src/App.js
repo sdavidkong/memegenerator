2f9b0bc697a23016226fb23032d16c6a6f62443d
S David Kong <SDAVIDKONG@gmail.com>
2022-05-31 19:00:04 +0200

    state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import './App.css';
+import './index.css';
 import Header from "./components/header.js"
 import Meme from "./components/meme.js"
 
--- src/components/meme.js
+++ src/components/meme.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import dogeface from "../dogeface.jpg"
+import memesData from "./memeData"
+import "../index.css"
 
 
 export default function Meme() {
+
+    let url
+    const [memeImage, setMemeImage] = useState(""); 
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() *memesArray.length)
+        
+        url = memesArray[randomNumber].url
+        setMemeImage(url)
+    
+    }
+
+    
     return (
         <main>
-            <form className="form">
+            <p>{url}</p>
+            <div className="form">
                 <input type="text" placeholder="top text" className="forminput"/>
                 <input type="text" placeholder="bottom text" className="forminput"/>
-                <button className="formbutton">Get a new meme image 🖼</button>
-            </form>
+                <button 
+                    onClick={getMemeImage}
+                    className="formbutton">
+                    Get a new meme image 🖼
+                    </button>
+            </div>
+            <img className="memeimage" src= {memeImage}/>
         </main>
     )
 }
